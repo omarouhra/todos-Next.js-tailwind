c39d1ebc4bd8974afbce55477fa611f9a35958ea
Omar Ouhra <omarmaroc26@gmail.com>
2021-08-23 11:50:56 +0100

    Input & States

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,56 @@
-import Head from 'next/head'
+// TODO create an input
+// TODO add the target value to an array
+
+import Head from "next/head";
+import { useState } from "react";
 
 export default function Home() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const inputTextHandler = e => {
+    setInputText(e.target.value);
+  };
+
+  const submitForm = e => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      {
+        text: inputText,
+        isCompleted: false,
+        id: Math.random() * 10,
+      },
+    ]);
+
+    setInputText("");
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
+        <form className='w-[400px]'>
+          <input
+            className='border-b border-black w-full focus:outline-none text-5xl '
+            type='text'
+            placeholder='Enter your task'
+            value={inputText}
+            onChange={inputTextHandler}
+          />
+          <button hidden type='submit' onClick={submitForm}>
+            GO
+          </button>
+        </form>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {todos.map(todo => (
+          <p key={todo.id}>{todo.text}</p>
+        ))}
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
